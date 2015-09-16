c395aae1272de5cdfde8deffd80804ee4577f271
tiago_missao <t.missao@gmail.com>
2015-09-15 22:06:44 -0300

    Add Layout and Functions

--- app/src/main/java/br/com/b_easy/Activity/MainActivity.java
+++ app/src/main/java/br/com/b_easy/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.b_easy.Activity;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -11,8 +12,13 @@ import android.widget.Toast;
 
 import com.melnykov.fab.FloatingActionButton;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.b_easy.Fragment.HomeFragment;
 import br.com.b_easy.Fragment.TaskFragment;
+import br.com.b_easy.Model.Subject;
 import br.com.b_easy.R;
 import br.liveo.Model.HelpLiveo;
 import br.liveo.interfaces.OnItemClickListener;
@@ -23,19 +29,28 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
 
     private HelpLiveo mHelpLiveo;
     private FloatingActionButton faButton;
+    private List<Subject> subjects;
+    private int INITIAL_INDEX_TASKS;
+    private int FINAL_INDEX_TASKS;
+
+    private final String TAG_HOME = "HOME";
+    private final String TAG_TASK = "TASK";
+    private final String SUBJECT_KEY = "NEW_SUBJECT";
+
+    private Subject selectedSubject;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        //super.setChildActivity(this);
+    protected void onCreate(final Bundle savedInstanceState) {
+        Log.d("FIRST", "onCreateFirst");
         super.setChildLayoutRes(R.layout.activity_main);
         super.setChildToolbarRes(R.id.app_bar);
         super.onCreate(savedInstanceState);
 
         if(savedInstanceState == null){
-            trocaFragment("home");
+            trocaFragment(TAG_HOME);
         }
 
-        /****************************************
+         /***************************************
          *       Seta Float Action Button       *
          ***************************************/
 
@@ -43,11 +58,17 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
         faButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Click Float Action Button ", Toast.LENGTH_SHORT).show();
+                subjects.add(new Subject("Teste"));
+                Toast.makeText(MainActivity.this, "Number of Subjects: " + subjects.size(), Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                i.putExtra(SUBJECT_KEY,new Subject("Teste"));
+                MainActivity.this.startActivity(i);
+                MainActivity.this.finish();
             }
         });
 
-
+        INITIAL_INDEX_TASKS = 2;
+        FINAL_INDEX_TASKS = INITIAL_INDEX_TASKS + subjects.size() -1 ;
 
     }
 
@@ -58,6 +79,7 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
 
     public void onInt(Bundle savedInstanceState) {
 
+        Log.d("FIRST", "onIntFirst");
         // User Information
         this.userName.setText("Tiago Missão");
         this.userEmail.setText("t.missao@gmail.com");
@@ -73,11 +95,25 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
          *        Pegar do Banco de Dados       *
          ***************************************/
 
-        mHelpLiveo.addColor(getString(R.string.drawer_pag_IA), R.drawable.ic_navigate_next_white_24dp, R.color.primaryTextColor);
-        mHelpLiveo.addColor(getString(R.string.drawer_pag_BD), R.drawable.ic_navigate_next_white_24dp, R.color.primaryTextColor);
-        mHelpLiveo.addColor(getString(R.string.drawer_pag_SO), R.drawable.ic_navigate_next_white_24dp, R.color.primaryTextColor);
-        mHelpLiveo.addColor(getString(R.string.drawer_pag_MD), R.drawable.ic_navigate_next_white_24dp, R.color.primaryTextColor);
-        mHelpLiveo.addColor(getString(R.string.drawer_pag_Calc), R.drawable.ic_navigate_next_white_24dp,  R.color.primaryTextColor);
+        subjects = new ArrayList<Subject>(){
+            {
+                add(new Subject(getString(R.string.drawer_pag_IA)));
+                add(new Subject(getString(R.string.drawer_pag_BD)));
+                add(new Subject(getString(R.string.drawer_pag_MD)));
+                add(new Subject(getString(R.string.drawer_pag_SO)));
+                add(new Subject(getString(R.string.drawer_pag_Calc)));
+            }
+        };
+
+        Subject s = (Subject) getIntent().getSerializableExtra(SUBJECT_KEY);
+        if(s != null){
+            Log.d("PARSE", s.getName());
+            subjects.add(s);
+        }
+
+        for(Subject aux : subjects){
+            mHelpLiveo.addColor(aux.getName(), R.drawable.ic_navigate_next_white_24dp, R.color.primaryTextColor);
+        }
 
         mHelpLiveo.addSeparator();
 
@@ -112,13 +148,21 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
     public void onItemClick(int position) {
         Log.d("Item Clicked", "Position " + position);
 
+        if(position == 0)
+            trocaFragment(TAG_HOME);
+        else if(position == 1)
+            return;
+        else if( !subjects.isEmpty() && position >= INITIAL_INDEX_TASKS && position <= FINAL_INDEX_TASKS){
+            setSelectedSubject(subjects.get(getSubjectIndexOnList(position)));
+            trocaFragment(TAG_TASK);
+        }
+        else{
+            // Fragment About
+        }
+
         super.setCurrentPosition(position);
         super.setCheckedItemNavigation(position,true);
 
-        if(position == 0)
-            trocaFragment("home");
-        else
-            trocaFragment("task");
     }
 
     /****************************************
@@ -144,13 +188,16 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
      ***************************************/
 
     public void trocaFragment(String tag){
+
         Fragment fragment;
 
-        if(tag.equals("home"))
+        if(tag.equals(TAG_HOME)) {
             fragment = new HomeFragment();
+        }
         else{
             fragment = new TaskFragment();
         }
+
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.main_container,fragment,tag).commit();
 
@@ -176,5 +223,17 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public Subject getSelectedSubject() {
+        return selectedSubject;
+    }
+
+    public void setSelectedSubject(Subject selectedSubject) {
+        this.selectedSubject = selectedSubject;
+    }
+
+    public int getSubjectIndexOnList(int position){
+        return position - INITIAL_INDEX_TASKS;
     }
 }
--- app/src/main/java/br/com/b_easy/Fragment/TaskFragment.java
+++ app/src/main/java/br/com/b_easy/Fragment/TaskFragment.java
@@ -2,10 +2,16 @@ package br.com.b_easy.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import br.com.b_easy.Activity.MainActivity;
+import br.com.b_easy.Model.Subject;
 import br.com.b_easy.R;
 
 /**
@@ -13,15 +19,30 @@ import br.com.b_easy.R;
  */
 public class TaskFragment extends Fragment {
 
+    private Subject subject;
+    private TextView tvCardTitle;
+    private MainActivity context;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null){
+
+        }
+
         setRetainInstance(true);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_task, container, false);
 
-        View v = inflater.inflate(R.layout.fragment_task,container,false);
+        context = ((MainActivity)getActivity());
+        subject = context.getSelectedSubject();
+
+        tvCardTitle = (TextView) v.findViewById(R.id.tvTitleCardFragmentTask);
+        tvCardTitle.setText(subject.getName());
+
         return v;
     }
 
